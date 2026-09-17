1c38c20aa772c4e95c319253d0fe97aea2f6967f
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2026-09-17 04:01:32 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,22 @@
 import net from "net";
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  const response = Buffer.alloc(8);
-  response.writeUInt32BE(0, 0);
-  response.writeUInt32BE(7, 4);
+  let buffer = Buffer.alloc(0);
 
-  connection.end(response);
+  connection.on("data", (chunk: Buffer) => {
+    buffer = Buffer.concat([buffer, chunk]);
+
+    if (buffer.length < 12) {
+      return;
+    }
+
+    const correlationId = buffer.readInt32BE(8);
+    const response = Buffer.alloc(8);
+    response.writeUInt32BE(0, 0);
+    response.writeInt32BE(correlationId, 4);
+
+    connection.end(response);
+  });
 });
 
 server.listen(9092, "127.0.0.1");
