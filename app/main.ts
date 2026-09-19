75ce220000bb7989a3dd69c905a5be8006a836c6
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2026-09-20 01:57:52 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -106,11 +106,11 @@ function readMetadataLog(): Map<string, TopicMetadata> {
   let offset = 0;
   const partitionsByTopic = new Map<string, PartitionMetadata[]>();
 
-  while (offset + 65 <= log.length) {
+  while (offset + 61 <= log.length) {
     const batchLength = log.readInt32BE(offset + 8);
     const batchEnd = offset + 12 + batchLength;
-    const recordsCount = log.readInt32BE(offset + 61);
-    let recordOffset = offset + 65;
+    const recordsCount = log.readInt32BE(offset + 57);
+    let recordOffset = offset + 61;
 
     if (batchLength <= 0 || batchEnd > log.length || recordsCount < 0) {
       break;
